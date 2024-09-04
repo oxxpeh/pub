c5ed47f3651fe2c212e46a75438c37e2f31bb9a5
oxxpeh <145547859+oxxpeh@users.noreply.github.com>
2024-09-05 00:23:38 +0900

    Update rdp-cl-in.sh

--- rdp-cl-in.sh
+++ rdp-cl-in.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" > /etc/apt/sources.list.d/mozilla.list
 apt update && apt install -y firefox
 apt install -y $( apt list --upgradable 2> /dev/null| grep "/" | sed -s "s@/.*@@")
