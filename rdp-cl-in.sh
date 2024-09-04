97b7d8ae406a3940227bfc55b89d86e7a2791319
oxxpeh <145547859+oxxpeh@users.noreply.github.com>
2024-09-05 00:25:08 +0900

    Update rdp-cl-in.sh

--- rdp-cl-in.sh
+++ rdp-cl-in.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" > /etc/apt/sources.list.d/mozilla.list
+apt remove -y firefox
 apt update && apt install -y firefox
 apt install -y $( apt list --upgradable 2> /dev/null| grep "/" | sed -s "s@/.*@@")
 apt autoremove -y 
