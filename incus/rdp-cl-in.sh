12ae196535ebb783c4ac416b571288e1df72ea99
oxxpeh <145547859+oxxpeh@users.noreply.github.com>
2024-09-05 19:35:54 +0900

    Update rdp-cl-in.sh

--- incus/rdp-cl-in.sh
+++ incus/rdp-cl-in.sh
@@ -2,7 +2,7 @@
 export DEBIAN_FRONTEND=noninteractive
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" > /etc/apt/sources.list.d/mozilla.list
 apt remove -y firefox
-apt update && apt install -y firefox
+apt update && apt install -y firefox xrdp lxqt
 apt install -y $( apt list --upgradable 2> /dev/null| grep "/" | sed -s "s@/.*@@")
 apt autoremove -y 
 
@@ -30,7 +30,7 @@ cat << EOF > host-start.sh
 #!/bin/bash
 #-- 2024/09/05
 echo -e "@-- start\t\$( date )" >> /home/work/host-start-stop.log
-mount.cifs //172.16.200.54/data/ /mnt/cifs/ -o user=user,password=xxx,uid=user,gid=user
+mount.cifs //192.168.1.10/data/ /mnt/cifs/ -o user=user,password=xxx,uid=user,gid=user
 EOF
 
 cat << EOF > host-stop.sh
