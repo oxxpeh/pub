1d265cb9d51e7654c5c0709167baf405299f318d
oxxpeh <145547859+oxxpeh@users.noreply.github.com>
2024-10-29 19:30:10 +0900

    Add files via upload

---
+++ incus/ubuntu24.10-xrdp/cl-rdp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+curl https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | \
+    sudo tee /etc/apt/sources.list.d/google-chrome.list
+apt remove -y avahi-daemon
+# apt-get -y update
+apt-get install -U -y --no-install-recommends google-chrome-stable
+
+sed -i "45s/oracular-backports/oracular-backports oracular-proposed/" /etc/apt/sources.list.d/ubuntu.sources
+apt install -U -y xrdp/oracular-proposed
+
+# apt install -y $( apt list --upgradable 2> /dev/null| grep "/" | sed -s "s@/.*@@")
+apt autoremove -y
+
+
+echo "@ -- cl-rdp end"
