ffdcc503e3f7042ad507f9086b68ec730e68eb21
oxxpeh <145547859+oxxpeh@users.noreply.github.com>
2024-11-29 01:07:36 +0900

    Create cl-init.sh

---
+++ kvm/at/cl-init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#echo 'user:Password' | chpasswd
+echo 'root:Password' | chpasswd
+
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt -y install apache2 dstat htop
+apt autoremove -y
+apt remove -y
