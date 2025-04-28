6513060df09fde75430072b5904d2a2eb2c96fc4
oxxpeh <145547859+oxxpeh@users.noreply.github.com>
2025-04-29 03:55:42 +0900

    Update pyeditor_add.js

--- javascript/2025_04_pyeditor_add/pyeditor_add.js
+++ javascript/2025_04_pyeditor_add/pyeditor_add.js
@@ -1,13 +1,13 @@
-// -- ver 0.01
-// -- 2025/04/27
+// -- ver 0.02
+// -- 2025/04/29
 // -- 2025/04/27
 
 // ページが読み込まれたときに実行される関数
 let aai_fail = 0;
 var timer1 = null;
+
 const ele_d_add = document.getElementById("elep_d_add");
-//let input; // inputをグローバルスコープで宣言
-//let executeButton; // executeButtonをグローバルスコープで宣言
+
 const add_html = `<br /><label for="elep_ll_tgt">target:</label>
 <select id="elep_ll_tgt">
     <option value="elep_pe_0" selected>ed_pe_0</option>
@@ -48,139 +48,6 @@ const add_html = `<br /><label for="elep_ll_tgt">target:</label>
 <br />&#91id:elep_d_log&#93<br />
 <div id="elep_d_log" style="height:200px;overflow-y: scroll;background-color: #E0E0E0"></div><br />`;
 
-function ffp_get_dt(){
-    // 現在の日時を取得
-    const now = new Date();
-    
-    // 現在の時間をフォーマット
-    const hours = String(now.getHours()).padStart(2, '0'); // 時間
-    const minutes = String(now.getMinutes()).padStart(2, '0'); // 分
-    const seconds = String(now.getSeconds()).padStart(2, '0'); // 秒
-    const milliseconds = String(now.getMilliseconds()).padStart(3, '0'); // ミリ秒
-    // フォーマットされた時間を作成
-    const aas_dtn = `${hours}:${minutes}:${seconds}.${milliseconds}`;
-    return aas_dtn
-}
-function ffp_wr_log(aas_log, ele_id = 'elep_d_log') {
-    // 現在の日時を取得
-    const currentTime = `${ffp_get_dt()}`
-
-    // 指定されたIDの要素に時間を出力
-    document.getElementById(ele_id).innerHTML += `${currentTime} ${aas_log} <br />`;
-    ele_id.scrollTop = ele_id.scrollHeight;
-}
-
-// get url
-function ffp_get(aas_url, ele_id_t = 'elep_pe_1', ele_id_o = 'elep_d_log') {
-    return fetch(aas_url)  // Promiseを返す
-    .then(res => {
-        if (!res.ok) {
-            throw new Error('nw err: ' + res.status);
-        }
-        return res.text();  // レスポンスをテキストとして取得
-    })
-    .then(text => {
-        document.getElementById(ele_id_t).code = text;  
-        // 取得したテキストを指定された要素に設定
-        //document.getElementById(ele_id_o).innerHTML = "to " + ele_id_t + " copy<br />";  
-        ffp_wr_log("to " + ele_id_t + " copy<br />");
-        // メッセージを表示
-    })
-    .catch(error => {
-        console.error('エラー:', error);
-    });
-}
-
-// checkURL
-function ffp_is_url(url) {
-    const regex = /^(https?:\/\/[^\s$.?#].[^\s]*)$/i;
-    return regex.test(url);
-}    
-function ffp_ch_url() {
-    const aas_url = document.getElementById('elep_ip_url').value;
-    if (aas_url && ffp_is_url(aas_url)) {
-        ffp_get(aas_url).then(() => {
-        document.getElementsByClassName('absolute py-editor-run-button')[0].click();
-    });
-    } //else {
-    //    alert("invalid URL");
-    //}
-}
-// ボタンを確認するための関数
-function ffp_ch_bt() {
-    const ele_bt = document.getElementsByClassName("absolute py-editor-run-button")[0];
-    
-    // ボタンが存在する場合
-    if (ele_bt) {
-        ele_bt.click(); // ボタンをクリック
-        clearInterval(timer1); // ループを停止
-        //ffp_ch_url(); // URLを変更する関数を呼び出す
-    } else {
-        // ボタンが見つからない場合
-        aai_fail++;
-        console.log("ボタンが見つかりません。1秒後に再試行します。");
-
-        // 5回失敗した場合、ループを停止
-        if (aai_fail >= 5) {
-            clearInterval(timer1);
-            console.log("ボタンが見つからなかったため、処理を終了します。");
-        }
-    }
-}
-// log出力
-function ffp_wr_io() {
-    const aas_tgt = document.getElementById("elep_ll_tgt").value;
-    const aai_no = Number(aas_tgt.charAt(aas_tgt.length - 1));
-    const ele_tgt = document.getElementById(aas_tgt);
-    const ele_tgto = document.getElementsByClassName("py-editor-output")[aai_no];
-    const aas_dt = ffp_get_dt();
-    var aas_out = `[${aas_dt}]`
-    aas_out += `lastout:<br />`
-    aas_out += `<pre><code>${ele_tgto.innerText}</code></pre>`
-    aas_out += `in:<br />`
-    aas_out += `<pre><code>${ele_tgt.code}</code></pre>`
-    //console.log(aas_out)
-    ele_d_log= document.getElementById("elep_d_log")
-    ele_d_log.innerHTML += aas_out;
-    //document.getElementById("elep_d_log").innerHTML += aas_out;
-    ele_d_log.scrollTop = ele_d_log.scrollHeight;
-}
-
-// ショートカットキー追加
-function ffp_shk_r() {
-    const ele_bt_tgt = document.getElementsByClassName("absolute py-editor-run-button")[0];
-    //alert("Ctrl + @ が押されました！");
-    //ffp_wr_io();
-    ele_bt_tgt.click()
-}
-
-
-// キー入力イベントを監視します
-document.addEventListener('keydown', function(event) {
-    // Ctrlキーが押されているか確認します
-    if (event.ctrlKey && event.key === '@') {
-        event.preventDefault(); // デフォルトの動作を防ぎます
-        ffp_shk_r(); // 関数を実行します
-    }
-});    
-
-//function ffp_mk_obj() {
-//    // 入力ボックスを作成
-//    input = document.createElement("input");
-//    input.type = "text";
-//    input.placeholder = "ここに入力";
-//
-//    // 実行ボタンを作成
-//    executeButton = document.createElement("button");
-//    executeButton.textContent = "実行";
-//
-//    // ボタンのクリックイベントを追加
-//    executeButton.addEventListener("click", function() {
-//        const inputValue = input.value;
-//        alert("入力された値: " + inputValue);
-//    });
-//
-//};
 function ffp_add_obj() {
     //ele_d_py_add.appendChild(input);
     //ele_d_py_add.appendChild(executeButton);
@@ -280,7 +147,7 @@ function ffp_add_obj() {
             const url = URL.createObjectURL(blob);
             const a = document.createElement('a');
             a.href = url;
-            a.download = 'pye.log';
+            a.download = 'pys.html';
             document.body.appendChild(a);
             a.click();
             document.body.removeChild(a);
@@ -294,10 +161,145 @@ function ffp_add_obj() {
     aas_url = document.getElementById("elep_pe_1").src;
     document.getElementById("elep_ip_url").value = aas_url;
 };
+
+
+// ショートカットキー追加
+function ffp_add_shk() {
+    //const ele_bt_tgt = document.getElementsByClassName("absolute py-editor-run-button")[0];
+    const ele_bt_tgt = document.getElementsById("elep_bt_e");
+    //alert("Ctrl + @ が押されました！");
+    //ffp_wr_io();
+    ele_bt_tgt.click()
+}
+
+// ボタンを確認するための関数
+function ffp_ch_bt() {
+    const ele_bt = document.getElementsByClassName("absolute py-editor-run-button")[0];
+    
+    // ボタンが存在する場合
+    if (ele_bt) {
+        ele_bt.click(); // ボタンをクリック
+        clearInterval(timer1); // ループを停止
+        //ffp_ch_url(); // URLを変更する関数を呼び出す
+    } else {
+        // ボタンが見つからない場合
+        aai_fail++;
+        console.log("ボタンが見つかりません。1秒後に再試行します。");
+
+        // 5回失敗した場合、ループを停止
+        if (aai_fail >= 5) {
+            clearInterval(timer1);
+            console.log("ボタンが見つからなかったため、処理を終了します。");
+        }
+    }
+}
+
+function ffp_ch_url() {
+    const aas_url = document.getElementById('elep_ip_url').value;
+    if (aas_url && ffp_is_url(aas_url)) {
+        ffp_get(aas_url).then(() => {
+        document.getElementsByClassName('absolute py-editor-run-button')[0].click();
+    });
+    } //else {
+    //    alert("invalid URL");
+    //}
+}
+
+// get url
+function ffp_get(aas_url, ele_id_t = 'elep_pe_1', ele_id_o = 'elep_d_log') {
+    return fetch(aas_url)  // Promiseを返す
+    .then(res => {
+        if (!res.ok) {
+            throw new Error('nw err: ' + res.status);
+        }
+        return res.text();  // レスポンスをテキストとして取得
+    })
+    .then(text => {
+        document.getElementById(ele_id_t).code = text;  
+        // 取得したテキストを指定された要素に設定
+        //document.getElementById(ele_id_o).innerHTML = "to " + ele_id_t + " copy<br />";  
+        ffp_wr_log("to " + ele_id_t + " copy<br />");
+        // メッセージを表示
+    })
+    .catch(error => {
+        console.error('エラー:', error);
+    });
+}
+
+function ffp_get_dt(){
+    // 現在の日時を取得
+    const now = new Date();
+    
+    // 現在の時間をフォーマット
+    const hours = String(now.getHours()).padStart(2, '0'); // 時間
+    const minutes = String(now.getMinutes()).padStart(2, '0'); // 分
+    const seconds = String(now.getSeconds()).padStart(2, '0'); // 秒
+    const milliseconds = String(now.getMilliseconds()).padStart(3, '0'); // ミリ秒
+    // フォーマットされた時間を作成
+    const aas_dtn = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    return aas_dtn
+}
+
+function ffp_get_logo(aas_log){
+    aas_logo = aas_log.replace(/&/g, '&amp;');
+    aas_logo = aas_logo.replace(/\(/g, '&#40;').replace(/\)/g, '&#41;');
+    aas_logo = aas_logo.replace(/\[/g, '&#91;').replace(/\]/g, '&#93;');
+    aas_logo = aas_logo.replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
+    aas_logo = aas_logo.replace(/\{/g, '&#123;').replace(/}/g, '&#125;');
+    aas_logo = aas_logo.replace(/\//g, '&#47;').replace(/\\/g, '&#92;');
+    aas_logo = aas_logo.replace(/\-/g, '&#8722;').replace(/\=/g, '&#61;');
+    aas_logo = aas_logo.replace(/\,/g, '&#44;').replace(/\./g, '&#46;')
+    aas_logo = aas_logo.replace(/\|/g, '&#124;').replace(/\ /g, '&nbsp;');
+    return aas_logo
+}
+// checkURL
+function ffp_is_url(url) {
+    const regex = /^(https?:\/\/[^\s$.?#].[^\s]*)$/i;
+    return regex.test(url);
+}    
+
+// log出力
+function ffp_wr_io() {
+    const aas_tgt = document.getElementById("elep_ll_tgt").value;
+    const aai_no = Number(aas_tgt.charAt(aas_tgt.length - 1));
+    const ele_tgt = document.getElementById(aas_tgt);
+    const ele_tgto = document.getElementsByClassName("py-editor-output")[aai_no];
+    const aas_dt = ffp_get_dt();
+    const aas_o = ffp_get_logo(ele_tgto.innerText);
+    var aas_out = `[${aas_dt}]`
+    aas_out += `lastout:<br />`
+    aas_out += `<pre><code>${aas_o}</code></pre>`
+    aas_out += `in:<br />`
+    aas_out += `<pre><code>${ele_tgt.code}</code></pre>`
+    //console.log(aas_out)
+    ele_d_log= document.getElementById("elep_d_log")
+    ele_d_log.innerHTML += aas_out;
+    //document.getElementById("elep_d_log").innerHTML += aas_out;
+    ele_d_log.scrollTop = ele_d_log.scrollHeight;
+}
+
+function ffp_wr_log(aas_log, ele_id = 'elep_d_log') {
+    // 現在の日時を取得
+    const currentTime = `${ffp_get_dt()}`
+
+    // 指定されたIDの要素に時間を出力
+    document.getElementById(ele_id).innerHTML += `${currentTime} ${aas_log} <br />`;
+    ele_id.scrollTop = ele_id.scrollHeight;
+}
+
+// キー入力イベントを監視します
+document.addEventListener('keydown', function(event) {
+    // Ctrlキーが押されているか確認します
+    if (event.ctrlKey && event.key === '@') {
+        event.preventDefault(); // デフォルトの動作を防ぎます
+        ffp_add_shk(); // 関数を実行します
+    }
+});    
+
 window.onload = function() {
     //ffp_mk_obj();
     ffp_add_obj();
     ffp_wr_log('Page load', 'elep_d_log');
-    timer1 = setInterval(ffp_ch_bt, 1000);    
+    // timer1 = setInterval(ffp_ch_bt, 1000);    
     //end
 };
